a437770425bfd3b947e6cbe92e8bebc1e4f28c48
Yassine <79665598+yassinemakhlouf1@users.noreply.github.com>
2022-07-04 22:29:39 +0100

    yassine

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,16 @@ import {BrowserRouter,Routes,Route} from "react-router-dom";
 import React from "react";
 import { Outlet } from 'react-router-dom';
 import Header from './static/Header';
+import ProductDetails from './Components/productdetails/ProductDetails';
 function App() {
   return (
     <>
     <BrowserRouter>
       <Routes >
         <Route path="/" element={<Layout />} >
+          <Route path="/Products/test" element={<ProductDetails/>}/>
         </Route>
-         
-          
         </Routes>
-
-        
       </BrowserRouter>
     </>
   );
---
+++ client/src/Components/productdetails/ProductDetails.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import './style.css';
+export default function ProductDetails() {
+  return (
+    <div className='Product-container'>
+    <div>
+      <div className='image-container'><img src="/Product.png" alt=""/></div>
+      <div className='image-container'><img src="/Product.png" alt=""/></div>
+      <div className='image-container'><img src="/Product.png" alt=""/></div>
+    </div>
+    <div className='Product-container-details'>
+    <div className='image-container-Big'><img src="/Product.png" alt=""/></div>
+    <div className='DetailProd'>
+      <div className='title'>Appolo</div>
+      <div className='Soustitle'>Running Short</div>
+      <div className='Size'>Size:</div>
+      <div className='SizeBox'>
+        <div className='Box'>XS</div>
+        <div className='Box'>S</div>
+        <div className='Box'>L</div>
+        <div className='Box'>XL</div>
+      </div>
+      <div className='Size'>color:</div>
+      <div className='ColBox'>
+        <div className='ColorBox1'></div>
+        <div className='ColorBox2'></div>
+        <div className='ColorBox3'></div>
+      </div>
+      <div className='Size'>Price:</div>
+      <div className="price">$50.00</div>
+      <button className='button' >ADD TO CART</button>
+      <div className='textDet'>Find stunning women's cocktail dresses and party dresses.
+      Stand out in lace and metallic cocktail
+      dresses and party dresses from all your favorite brands.</div>
+    </div>
+    </div>
+    </div>
+  )
+}
