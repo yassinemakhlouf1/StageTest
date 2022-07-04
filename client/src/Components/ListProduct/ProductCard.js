800d71fb62f4adf0e9779015d48dc531e64be823
Yassine <79665598+yassinemakhlouf1@users.noreply.github.com>
2022-07-05 00:10:23 +0100

    update

--- client/src/Components/ListProduct/ProductCard.js
+++ client/src/Components/ListProduct/ProductCard.js
@@ -3,11 +3,11 @@ import './ProductCard.css';
 
 export default function ProductCard({title, imageUrl, price}) {
     return (
-<div class='card' className='card-container'>
+<div className='card-container'>
         <img className='image-container' src={imageUrl} alt=''  />
         <div className="content-container">
-            <label class='title'>{title}</label>
-            <label class='price'>{price}</label>
+            <label className='title'>{title}</label>
+            <label className='price'>{price}</label>
 </div>
 </div>
     )}
--- client/src/Components/productdetails/ProductDetails.js
+++ client/src/Components/productdetails/ProductDetails.js
@@ -1,35 +1,40 @@
-import React from 'react'
-import './ProdDetail.css';
+import React, { useEffect } from "react";
+import "./prodDetail.css";
 export default function ProductDetails() {
+
+  const  test = ()=> {
+    console.log("test");
+  }
+
   return (
-    <div className='Product-container'>
+    <div className="Product-container">
     <div>
-      <div className='image-container'><img src="/Product.png" alt=""/></div>
-      <div className='image-container'><img src="/Product.png" alt=""/></div>
-      <div className='image-container'><img src="/Product.png" alt=""/></div>
+      <div className="image-containerr"><img src="/Product.png" alt=""/></div>
+      <div className="image-containerr"><img src="/Product.png" alt=""/></div>
+      <div className="image-containerr"><img src="/Product.png" alt=""/></div>
     </div>
-    <div className='Product-container-details'>
-    <div className='image-container-Big'><img src="/Product.png" alt=""/></div>
-    <div className='DetailProd'>
-      <div className='title'>Appolo</div>
-      <div className='Soustitle'>Running Short</div>
-      <div className='Size'>Size:</div>
-      <div className='SizeBox'>
-        <div className='Box'>XS</div>
-        <div className='Box'>S</div>
-        <div className='Box'>L</div>
-        <div className='Box'>XL</div>
+    <div className="Product-container-details">
+    <div className="image-container-Big"><img src="/Product.png" alt=""/></div>
+    <div className="DetailProd">
+      <div className="titlee">Appolo</div>
+      <div className="Soustitle">Running Short</div>
+      <div className="Size">Size:</div>
+      <div className="SizeBox">
+        <div className="Box">XS</div>
+        <div className="Box">S</div>
+        <div className="Box">L</div>
+        <div className="Box">XL</div>
       </div>
-      <div className='Size'>color:</div>
-      <div className='ColBox'>
-        <div className='ColorBox1'></div>
-        <div className='ColorBox2'></div>
-        <div className='ColorBox3'></div>
+      <div className="Size">color:</div>
+      <div className="ColBox">
+        <div className="ColorBox1"></div>
+        <div className="ColorBox2"></div>
+        <div className="ColorBox3"></div>
       </div>
-      <div className='Size'>Price:</div>
+      <div className="Size">Price:</div>
       <div className="price">$50.00</div>
-      <button className='button' >ADD TO CART</button>
-      <div className='textDet'>Find stunning women's cocktail dresses and party dresses.
+      <button className="button" onClick={test}  >ADD TO CART</button>
+      <div className="textDet">Find stunning women"s cocktail dresses and party dresses.
       Stand out in lace and metallic cocktail
       dresses and party dresses from all your favorite brands.</div>
     </div>
