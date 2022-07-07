1c8a31705d32494ef4543bdc6719cee8e01310fc
Yassine <79665598+yassinemakhlouf1@users.noreply.github.com>
2022-07-07 01:19:13 +0100

    updateItheb

--- client/src/Components/cart/Cart.js
+++ client/src/Components/cart/Cart.js
@@ -2,11 +2,14 @@ import React, { useState } from 'react';
 import "./style.css";
 export default function Cart() {
   const [count, setCount] = useState(1);
+var left= '<';
+var right='>';
 
   return (
     <div className='cart-container'>    <div className='cart'>Cart</div>
-    <div >
     <hr/>
+    <div >
+ 
     <div className='cart-container-detail'>
     <div className="DetailProdc">
       <div className="titleeC" >Appolo</div>
@@ -32,7 +35,48 @@ export default function Cart() {
       <p className='pCart'>{count}</p>
       <button className='buttonC' onClick={() => {if (count>0 ) setCount(count - 1)}}>-</button></div>
     </div>
-    <div className="image-containerC"><img src="/Product.png" alt=""/></div>
+    <div className="image-containerC">
+      <img src="/ProductGhaith.png" alt=""/>
+      <button className='btn'> {left} </button>
+      <button className='btn1'> {right} </button>
+      </div>
+    
+    </div>
+    </div><hr/>
+    
+    </div>
+    <div >
+    
+    <div className='cart-container-detail'>
+    <div className="DetailProdc">
+      <div className="titleeC" >Appolo</div>
+      <div className="SoustitleC">Running Short</div>
+      <div className="priceC">$50.00</div>
+      <div className="SizeC">Size:</div>
+      <div className="SizeBoxC">
+        <div className="BoxC">XS</div>
+        <div className="BoxC">S</div>
+        <div className="BoxC">L</div>
+        <div className="BoxC">XL</div>
+      </div>
+      <div className="SizeC">color:</div>
+      <div className="ColBoxC">
+        <div className="ColorBox1C"></div>
+        <div className="ColorBox2C"></div>
+        <div className="ColorBox3C"></div>
+      </div>
+      </div>
+      <div className='container-right-c'>
+      <div className='container-nb'>
+      <div><button className='buttonC' onClick={() => setCount(count + 1)}>+</button>
+      <p className='pCart'>{count}</p>
+      <button className='buttonC' onClick={() => {if (count>0 ) setCount(count - 1)}}>-</button></div>
+    </div>
+    <div className="image-containerC">
+      <img src="/ProductGhaith.png" alt=""/>
+      <button className='btn'> {left} </button>
+      <button className='btn1'> {right} </button>
+      </div>
     
     </div>
     </div><hr/>
