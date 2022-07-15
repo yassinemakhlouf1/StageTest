6d206f0026023ddcc54e3e82e563ba065e5ddac8
BedouiGhaith <98600281+BedouiGhaith@users.noreply.github.com>
2022-07-16 00:37:37 +0100

    day9

--- client/src/Components/ListProduct/product_list_api.js
+++ client/src/Components/ListProduct/product_list_api.js
@@ -4,14 +4,14 @@ import axios from 'axios';
 export const products_details = async (id) => {
     try {
     const { data } = await axios.post(
-        'http://localhost:4000',{"query":"{categories {products{id name gallery inStock prices{currency{symbol}} prices{amount}}}}"},{
+        'http://localhost:4000',{"query":"{categories {name products{id name gallery inStock prices{currency{symbol}} prices{amount}}}}"},{
             headers: {
               'Content-Type': 'application/json'
             }
           }
     );
 
-    return data.data.categories[0].products;
+    return data.data.categories;
   } catch (error) {
     console.log(error);
   }
