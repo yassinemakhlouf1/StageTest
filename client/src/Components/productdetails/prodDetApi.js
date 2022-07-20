8ad72447e0bcef48a4a44ae289aeb56a18702192
Yassine <79665598+yassinemakhlouf1@users.noreply.github.com>
2022-07-20 16:48:47 +0100

    finitionCarteDetails

--- client/src/Components/productdetails/prodDetApi.js
+++ client/src/Components/productdetails/prodDetApi.js
@@ -5,7 +5,7 @@ export const getDetailProd = async (id) => {
     try {
     const { data } = await axios.post(
       
-        'http://localhost:4000',{"query":"{product(id:"+'"'+id+'"' +"){name inStock gallery description  brand prices{currency{symbol}} prices{amount} }}"},{
+        'http://localhost:4000',{"query":"{product(id:"+'"'+id+'"' +"){id name inStock gallery description  brand prices{currency{symbol}} prices{amount} }}"},{
             headers: {
               'Content-Type': 'application/json'
             }
